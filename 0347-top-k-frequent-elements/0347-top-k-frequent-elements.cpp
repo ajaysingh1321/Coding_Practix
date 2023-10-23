eee31439b7408727902d84ea682a08c7d1bea41b
Ajay Singh <35740994+ajaysingh1321@users.noreply.github.com>
2023-10-23 20:39:23 +0530

    Time: 16 ms (28.9%), Space: 14.1 MB (44.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -7,8 +7,8 @@ public:
             mp[nums[i]]++;
         }
         priority_queue<pair<int, int>> pq;
-        for (auto entry : mp) {
-            pq.push({entry.second, entry.first});
+        for (auto i : mp) {
+            pq.push({i.second, i.first});
         }
         
         for (int i = 0; i < k; i++) {
